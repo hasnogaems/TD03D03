cf3dabca1d7348d3803b76c62aad09cdfe12470a
Medusa Galadriel <medusaga@su-a4.msk.21-school.ru>
2023-07-27 20:24:18 +0300

    arithmetic2

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-int sum(int a, int b);
-
 int main()
 {
-    return 0;
-}
+  int num1, num2;
+  int sum, diff, product, quotient;
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+  //validity Check
+  if(scanf("%d %d", &num1, &num2) != 2 || getchar() != '\n') {
+    printf("n/a\n");
+    return 0;
+  }
+
+  //Check if 0
+  if(num2 == 0) {
+    printf("n/a\n");
+    return 0;
+  }
+
+  //Compute
+  sum = num1 + num2;
+  diff = num1 -num2;
+  product = num1 * num2;
+  quotient = num1/num2;
+
+  printf("%d %d %d %d\n", sum, diff, product, quotient);
+  return 0;
 }
