0013a8ec3bd681343ba199d09a7818230582ed5c
Medusa Galadriel <medusaga@su-a4.msk.21-school.ru>
2023-07-27 18:10:58 +0300

    named_hello

---
+++ src/named_hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char name[100];
+  //Receive input from the user
+  scanf("%s", name);
+
+  //output to the terminal
+  printf("Hello, %s!\n", name);
+  return 0;
+}
